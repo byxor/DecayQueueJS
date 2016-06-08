8b7305caa7210eb047d6103ac26a2f75fa508a93
byxor <brandnewbyxor@gmail.com>
2016-06-08 19:14:06 +0100

    Create example code

---
+++ website/main.js
@@ -0,0 +1,30 @@
+var lifetime = 1000;
+var inputQueue = new DecayQueue(lifetime);
+
+function setup()
+{
+	frameRate(30);
+	createCanvas(600, 600);
+}
+
+function draw()
+{
+	inputQueue.update();
+	
+	background(255, 255, 255);
+	
+	
+	
+	for (var i = 0; i < inputQueue.getLength(); i++)
+	{
+		var input = inputQueue.dequeue();
+		text(input, 10, (i+4)*15);
+	}
+	
+}
+
+function keyPressed()
+{
+	inputQueue.enqueue(keyCode);
+}
+
